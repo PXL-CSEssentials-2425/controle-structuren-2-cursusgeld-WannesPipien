60792b9729b7d20e5b653734a56e5a17ac7dd5fc
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-17 11:50:13 +0200

    Applicatie wekt

---
+++ Cursus_geld/Cursus_geld/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cursus_geld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int jaartal;
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int hours = int.Parse(hoursTextBox.Text);
+            double amount = hours * 1.5;
+            int decenia = (jaartal % 400);
+            int century = (jaartal - (jaartal % 400));
+            if(decenia % 4 == 0 && century % 400 == 0)
+            {
+                amount += 1.5;
+                leapjearLabel.Content = "Is schrikkeljaar";
+            }
+
+            amountToPayTextBox.Text = amount.ToString();
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool nummeriek = int.TryParse(jearTextBox.Text, out jaartal);
+            if(nummeriek)
+            {
+                numberLabel.Content = "Is nummeriek";
+            }
+            else
+            {
+                numberLabel.Content = "Geef een correct jaartal!";
+            }
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
